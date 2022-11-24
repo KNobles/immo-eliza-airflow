663304fa172bd7cbad1836b854074982a8913aa2
KNobles <nobles-k@hotmail.com>
2022-11-24 16:19:16 +0100

    added upload and download files from cloud

--- dags/Dashboard/main.py
+++ dags/Dashboard/main.py
@@ -4,7 +4,13 @@ import plotly.express as px
 import plotly.graph_objects as go
 from dash import Dash, dcc, html, Input, Output 
 import dash_bootstrap_components as dbc
+from azure.storage.blob import BlobServiceClient
+from dotenv import load_dotenv
+from utils.download_file import download_file
 
+download_file("model/immo_model.pkl", "immo_model_akl.pkl")
+download_file("model/immo_scaler.pkl", "immo_scaler_akl.pkl")
+download_file("model/immo_poly_features.pkl", "immo_poly_features_akl.pkl")
 
 app = Dash(__name__, use_pages=True, external_stylesheets=[dbc.themes.SOLAR])
 server = app.server
