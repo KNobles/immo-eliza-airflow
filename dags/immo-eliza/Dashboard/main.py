2d0fdae90075cf169b1a878c3d211bc2a66651b3
Antoine Vanneste <113119888+gastoontje@users.noreply.github.com>
2022-11-25 11:45:01 +0100

    Update main.py

--- dags/immo-eliza/Dashboard/main.py
+++ dags/immo-eliza/Dashboard/main.py
@@ -8,9 +8,9 @@ from azure.storage.blob import BlobServiceClient
 from dotenv import load_dotenv
 from utils.download_file import download_file
 
-download_file("model/immo_model.pkl", "immo_model_akl.pkl")
-download_file("model/immo_scaler.pkl", "immo_scaler_akl.pkl")
-download_file("model/immo_poly_features.pkl", "immo_poly_features_akl.pkl")
+#download_file("model/immo_model.pkl", "immo_model_akl.pkl")
+#download_file("model/immo_scaler.pkl", "immo_scaler_akl.pkl")
+#download_file("model/immo_poly_features.pkl", "immo_poly_features_akl.pkl")
 
 app = Dash(__name__, use_pages=True, external_stylesheets=[dbc.themes.SOLAR])
 server = app.server
